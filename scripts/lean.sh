bfa7c4836cd2b3cbf8b32c0d5b840ffbda45affc
MilesPoupart <renjunyang007@126.com>
2024-01-07 21:14:55 +0800

    add luci-app-daed-next

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -48,9 +48,9 @@ rm -rf ../../customfeeds/packages/net/adguardhome
 svn export https://github.com/QiuSimons/OpenWrt-Add/trunk/luci-app-irqbalance
 
 # Add luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall
 
 # Add luci-app-netdata
 rm -rf ../../customfeeds/luci/applications/luci-app-netdata
@@ -67,7 +67,10 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-autotimeset
 sed -i "s/\"control\"/\"system\"/g" luci-app-autotimeset/luasrc/controller/autotimeset.lua
 
 # Add luci-app-ssr-plus
-git clone -b main --depth=1 https://github.com/fw876/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld
+
+# add luci-app-daed
+git clone https://github.com/sbwml/luci-app-daed-next
 
 # Add luci-app-unblockneteasemusic
 rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
