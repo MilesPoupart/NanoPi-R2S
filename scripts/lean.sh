ad3fa387e3c6c3e1938f7c99c6fc1d33f5dca3b5
MilesPoupart <renjunyang007@126.com>
2024-01-18 14:09:39 +0800

    update

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -194,8 +194,6 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='HarmonyWrt'' package/lean/default-settings/files/zzz-default-settings
 sed -i "s/OpenWrt /MilesPoupart @ HarmonyWrt /g" package/lean/default-settings/files/zzz-default-settings
 
-cat $GITHUB_WORKSPACE/xdp-sockets-diag_support.mk >> package/kernel/linux/modules/netsupport.mk
-
 rm package/base-files/files/etc/banner
 touch package/base-files/files/etc/banner
 echo -e "____________________________________________________________________" >> package/base-files/files/etc/banner
